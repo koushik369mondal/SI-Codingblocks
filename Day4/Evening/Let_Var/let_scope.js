7541f5da36d5b1a5e94be89ba494725540afaa57
Kaushik Mandal <koushik369mondal@gmail.com>
2026-07-06 14:40:16 +0530

    refactor let_scope.js to comment out variable hoisting examples for clarity

--- Day4/Evening/Let_Var/let_scope.js
+++ Day4/Evening/Let_Var/let_scope.js
@@ -1,10 +1,16 @@
-console.log(x);
-let x = 10; // error: Cannot access 'x' before initialization
-var x = 10; // undefined
-console.log(x);
+// console.log(x);
+// let x = 10; // error: Cannot access 'x' before initialization
+// var x = 10; // undefined
+// console.log(x);
 
-//hoisting
-var x;
-console.log(x);
-x = 10;
-console.log(x);
+// //hoisting
+// var x;
+// console.log(x);
+// x = 10;
+// console.log(x);
+
+let message = "Hello, Guys";
+console.log(message);
+
+message = "Hello, I am Kaushik Mandal"; // error: Identifier 'message' has already been declared
+console.log(message);

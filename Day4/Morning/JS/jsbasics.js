1a47b7d38a53deaba4609340834c8d9765a5b50b
Kaushik Mandal <koushik369mondal@gmail.com>
2026-07-06 11:39:03 +0530

    update animation duration for move2; add jsbasics.js with number printing examples

---
+++ Day4/Morning/JS/jsbasics.js
@@ -0,0 +1,27 @@
+var x = 1;
+console.log(x);
+
+x = "Hello, Guys";
+console.log(x);
+
+i = 20;
+
+while (i <= 30) {
+    console.log(i);
+    i++;
+}
+
+// print all no from 1 to 100
+for (var i = 1; i <= 100; i++) {
+    console.log(i);
+}
+
+// print all even no from 1 to 100
+for (var i = 2; i <= 100; i += 2) {
+    console.log(i);
+}
+
+// print all odd no from 1 to 100
+for (var i = 1; i <= 100; i += 2) {
+    console.log(i);
+}

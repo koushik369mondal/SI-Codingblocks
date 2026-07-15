99f256283465cdeb1427620d7a4d61711f9f31e1
Kaushik Mandal <koushik369mondal@gmail.com>
2026-07-15 11:21:49 +0530

    Refactor code structure for improved readability and maintainability

--- Day9/index.js
+++ Day9/index.js
@@ -1,10 +1,28 @@
 const http = require('http');
 
-//create server
+// //create server
+// const myserver = http.createServer((req, res) => {
+//     console.log("Hello")
+//     res.end('hello student');
+// })
+
+
 const myserver = http.createServer((req, res) => {
-    console.log("Hello")
-    res.end('hello student');
+    // console.log("Hello")
+    // res.end('hello student');
+    switch(req.url){
+        case '/' : res.end('hello from server');
+        break;
+        case '/contact' : res.end('contact page');
+        break;
+        case '/about' : res.end('I am in about page');
+        break;
+        default : res.end('404 error');
+        break;
+    }
 })
+
+
 
 
 // server listen port

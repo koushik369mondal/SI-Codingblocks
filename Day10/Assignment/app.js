1f5cf4d0b6e0c9690d0f50cf52dbd012ec69c5eb
Kaushik Mandal <koushik369mondal@gmail.com>
2026-07-16 19:18:48 +0530

    Add user API endpoint and middleware for JSON handling

--- Day10/Assignment/app.js
+++ Day10/Assignment/app.js
@@ -4,9 +4,20 @@ const fs = require('fs');
 const app = express();
 const PORT = 8001;
 
+let users = require("../Lec12/MOCK_DATA.json");
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
 // HOME
 app.get('/', (req, res)=>{
     res.send(`Server running on port ${PORT}`)
+})
+
+// GET All Users
+app.get('/api/users', (req, res) => {
+    return res.json(users);
 })
 
 

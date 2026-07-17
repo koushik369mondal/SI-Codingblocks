d538abdcb0045a8927c2a2694ce99e8fbafe0ec5
Kaushik Mandal <koushik369mondal@gmail.com>
2026-07-17 18:44:26 +0530

    update student IDs in JSON file and enhance user route for fetching specific user

--- Day11/Assignment/routes/user.routes.js
+++ Day11/Assignment/routes/user.routes.js
@@ -5,9 +5,19 @@ const router = express.Router();
 
 let users = require('../students.json');
 
+// GET all
 router.get('/', (req, res) => {
     return res.json(users);
 });
+
+// Get Particular
+router.get('/:id', (req, res) =>{
+    const id = req.params.id;
+    console.log(typeof(id));
+    const user = users.filter((item) => item.id === id);
+    console.log(user);
+    return res.json(user);
+})
 
 
 module.exports = router;
